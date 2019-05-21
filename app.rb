511ab67c9dc8bf99505ba1d7c74efc42fb4664e8
ADIL NAIMI <adilnm23@hotmail.com>
2019-05-21 09:15:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,10 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Adil Naimi"
   end
+  get '/hometown' do
+    "My hometown is Marrakech"
+  end
+  get '/favorite-song' do
+    "My favorite-song is Man in the Mirror"
+  end
 end
